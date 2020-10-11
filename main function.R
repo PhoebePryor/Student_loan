4ffde6a68cea73fbfb071427b14639ac3293290d
Phoebe Pryor-Hilliard <phoebelouisepryor@gmail.com>
2020-10-11 16:18:06 +0100

    comparaison

--- main function.R
+++ main function.R
@@ -2,22 +2,22 @@ repayment <- function(total_loan, salary_net, salary_increase, accumulated_pmt, 
   
   initial_salary <- salary_net
   initial_loan <- total_loan
+  t_total_loan <- total_loan
+  t_salary_net <- salary_net
+  t_accumulated <- accumulated_pmt
   
   for(mth in 1: 360){
     
     if (total_loan < 0){
       
-      return(
-        
-        paste("initial salary:", initial_salary, "    current salary:", salary_net, 
-              "    extra payment chosen:", extra_payment, "    month when loan paid off:", mth,
-              "    overall payment:", accumulated_pmt, "saved:", initial_loan - accumulated_pmt))
-      break
-    }
-    
-    else {
+      overall_pmt <<- accumulated_pmt
+      overall_mth <<- mth
       
-      monthly_payment <- ((salary_net - 21000)* 0.06/12) + extra_payment
+      break
+      
+    } else {
+      
+      monthly_payment <- ((salary_net - 21000)* 0.06/12) + 0
       
       interest_added = (total_loan - monthly_payment)*0.063 /12
       
@@ -26,9 +26,32 @@ repayment <- function(total_loan, salary_net, salary_increase, accumulated_pmt, 
       total_loan <- total_loan - monthly_payment + interest_added
       
       accumulated_pmt <- accumulated_pmt + monthly_payment
-      
     }
-    
   }
   
+  for(mth in 1:360){
+    
+    if (t_total_loan < 0){
+      
+       new_pmt <<- t_accumulated
+       new_mth <<- mth
+      
+       break
+       
+    } else {
+      
+      t_monthly_payment <- ((t_salary_net - 21000)* 0.06/12) + extra_payment
+      
+      t_interest_added = (t_total_loan - t_monthly_payment)*0.063 /12
+      
+      t_salary_net <- t_salary_net * ((salary_increase/12)+1)
+      
+      t_total_loan <- t_total_loan - t_monthly_payment + t_interest_added
+      
+      t_accumulated <- t_accumulated + t_monthly_payment
+      
+    }
+  }
+  paste("Overall saved", overall_pmt - new_pmt, "paid off", overall_mth - new_mth, "months quicker")
 }
+  
