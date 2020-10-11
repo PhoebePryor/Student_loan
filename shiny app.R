39541078d7fd7889dbcfc23074737b6ff4f964c8
Phoebe Pryor-Hilliard <phoebelouisepryor@gmail.com>
2020-10-11 20:11:46 +0100

    shiny_app

--- shiny app.R
+++ shiny app.R
@@ -1,1 +1,38 @@
-# shiny interactive app 
+# shiny interactive app
+
+library(shiny)
+library(dplyr)
+library(DT)
+library(ggplot2)
+library(shinydashboard)
+library(plotly)
+
+ui <- navbarPage("Student loan repayment",
+                 tabPanel("About", fluidPage(
+                   
+                   titlePanel("Hello Shiny!"),
+                   
+                   sidebarLayout(
+                     
+                     sidebarPanel(
+                
+                     ),
+                     mainPanel(
+                       plotOutput()
+                     )))),
+                 tabPanel("calculator", fluidPage(
+                   titlePanel("second")
+                 ))
+)
+
+
+
+
+  
+ 
+
+server <- function(input, output){
+  
+}
+
+shinyApp(ui = ui, server = server)
